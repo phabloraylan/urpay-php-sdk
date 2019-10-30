d4c3e7d02110837b5ff884a9a80d6561dbcf6be1
phabloraylan <phabloraylan@gmail.com>
2019-10-30 01:53:11 -0300

    Arquivos de testes

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -13,14 +13,39 @@ class ClientTest extends TestCase
         $this->assertFileExists($file);
     }
 
-    public function testTokenConfigSDKException()
+    public function testTokenConfigFileNotExistsException()
     {
         $this->expectException(URPaySDKException::class);
         $client = new Client();
-        $file = __DIR__ . "/files/urpay_tokens.json";
+        $file = __DIR__ . "/files/urpay_token.json";
         $client->setTokenConfig($file);
+    }
+
+
+    public function testTokenConfigJsonInvalidException()
+    {
+        $this->expectException(URPaySDKException::class);
+        $client = new Client();
 
         $file = __DIR__ . "/files/urpay_tokens_invalid.json";
         $client->setTokenConfig($file);
+    }
+
+    public function testTokenConfigFileNotExistFieldsException()
+    {
+        $this->expectException(URPaySDKException::class);
+        $client = new Client();
+
+        $file = __DIR__ . "/files/urpay_tokens_fields_invalid.json";
+        $client->setTokenConfig($file);
+    }
+
+    public function testUseApplicationDefaultTokens()
+    {
+        putenv('URPAY_APPLICATION_TOKENS=' . __DIR__ . "/files/urpay_tokens.json");
+        $client = new Client();
+        $client->useApplicationDefaultTokens();
+
+        $this->assertEquals("token-common", $client->getTokenCommon());
     }
 }
