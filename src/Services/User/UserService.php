1b9819ea3f336b40093d35f54993ff8542d19a1f
phabloraylan <phabloraylan@gmail.com>
2019-10-30 16:44:04 -0300

    Adicionado serviço de usuário

---
+++ src/Services/User/UserService.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace URPay\Services\User;
+
+use GuzzleHttp\Exception\ClientException;
+use URPay\Client;
+use URPay\Http\Api;
+use URPay\Http\Endpoint;
+use GuzzleHttp\Exception\ServerException;
+use URPay\Exceptions\URPayResponseException;
+use URPay\Exceptions\URPayTokenException;
+use URPay\Utils\MoneyUtil;
+
+class UserService extends Api
+{
+
+    private static $user;
+
+    private function __construct()
+    { }
+
+    public static function getUser(Client $client, $user)
+    {
+        if (!isset(self::$user)) {
+
+            $response = self::getResponse($client, $user);
+            $arr = self::fromJson($response)['user'];
+
+            self::$user = new User();
+            self::$user->setGender($arr['gender']);
+            self::$user->setId($arr['_id']);
+            self::$user->setUser($arr['user']);
+            self::$user->setName($arr['name']);
+
+            $document = Document();
+            $document->setDocument($arr['document']['document']);
+            $document->setType($arr['document']['type']);
+
+            self::$user->setDocument($document);
+
+            $address = new Address();
+            $address->setCity($arr['address']['city']);
+            $address->setState($arr['address']['state']);
+            $address->setCountry($arr['address']['country']);
+
+            self::$user->setAddress($address);
+        }
+
+        return self::$user;
+    }
+
+    public static function getResponse(Client $client, $user)
+    {
+        try {
+            $clientRest = self::getClientRest();
+
+            $response = $clientRest->request(self::GET, Endpoint::USER . '/' . $user, [
+                'headers' => [
+                    'Authorization' => $client->getTokenCommon()
+                ]
+            ]);
+        } catch (ClientException $e) {
+
+            if ($e->getCode() == 401) {
+                throw new URPayTokenException(self::getErrorMessage($e), $e->getCode());
+            }
+
+            throw new URPayResponseException(self::getErrorMessage($e), $e->getCode());
+        } catch (ServerException $e) {
+            throw new URPayServerException("Erro no servidor da URPay", $e->getCode());
+        }
+
+        return $response;
+    }
+}
